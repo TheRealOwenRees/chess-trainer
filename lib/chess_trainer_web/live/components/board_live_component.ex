c978a6d161fa21671289aa8c8a54919158cf0fbe
Owen Rees <owenrees.dev@gmail.com>
2025-10-26 22:01:02 +0100

    fix mirrored orientation

--- lib/chess_trainer_web/live/components/board_live_component.ex
+++ lib/chess_trainer_web/live/components/board_live_component.ex
@@ -5,21 +5,21 @@ defmodule ChessTrainerWeb.BoardLiveComponent do
 
   def render(assigns) do
     files =
-      case assigns.active_color do
-        :black -> [:a, :b, :c, :d, :e, :f, :g, :h]
-        :white -> Enum.reverse([:a, :b, :c, :d, :e, :f, :g, :h])
+      case assigns.game.active_color do
+        :white -> [:a, :b, :c, :d, :e, :f, :g, :h]
+        :black -> Enum.reverse([:a, :b, :c, :d, :e, :f, :g, :h])
       end
 
     ranks =
-      case assigns.active_color do
-        :black -> Enum.reverse(1..8)
-        :white -> 1..8
+      case assigns.game.active_color do
+        :white -> Enum.reverse(1..8)
+        :black -> 1..8
       end
 
     ~H"""
     <div class="w-[388px] m-auto left-0 right-0 mt-6">
       <div class="border border-2 border-zinc-700 grid grid-rows-8 grid-cols-8">
-        <%= for rank <- Enum.reverse(ranks) do %>
+        <%= for rank <- ranks do %>
           <%= for file <- files do %>
             <% square = {file, rank} %>
             <% piece = Map.get(@game.board, square) %>
@@ -40,8 +40,7 @@ defmodule ChessTrainerWeb.BoardLiveComponent do
 
   def update(%{fen: fen}, socket) do
     game = Chex.Parser.FEN.parse(fen)
-    active_color = game.active_color
-    {:ok, assign(socket, game: game, active_color: active_color)}
+    {:ok, assign(socket, game: game)}
   end
 
   defp background(file_index, rank_index) when rem(file_index + rank_index, 2) != 0,
