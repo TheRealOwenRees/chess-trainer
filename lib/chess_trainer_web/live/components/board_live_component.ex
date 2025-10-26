ca3808215d5a3e40e1bbb41e9be6167ce71e0702
Owen Rees <owenrees.dev@gmail.com>
2025-10-26 21:39:07 +0100

    board orientation based on first color to move

--- lib/chess_trainer_web/live/components/board_live_component.ex
+++ lib/chess_trainer_web/live/components/board_live_component.ex
@@ -4,15 +4,27 @@ defmodule ChessTrainerWeb.BoardLiveComponent do
   import ChessTrainerWeb.PieceComponent
 
   def render(assigns) do
+    files =
+      case assigns.active_color do
+        :black -> [:a, :b, :c, :d, :e, :f, :g, :h]
+        :white -> Enum.reverse([:a, :b, :c, :d, :e, :f, :g, :h])
+      end
+
+    ranks =
+      case assigns.active_color do
+        :black -> Enum.reverse(1..8)
+        :white -> 1..8
+      end
+
     ~H"""
     <div class="w-[388px] m-auto left-0 right-0 mt-6">
       <div class="border border-2 border-zinc-700 grid grid-rows-8 grid-cols-8">
-        <%= for rank_index <- Enum.reverse(0..7) do %>
-          <%= for file_index <- 0..7 do %>
-            <% file = Enum.at([:a, :b, :c, :d, :e, :f, :g, :h], file_index) %>
-            <% rank = rank_index + 1 %>
+        <%= for rank <- Enum.reverse(ranks) do %>
+          <%= for file <- files do %>
             <% square = {file, rank} %>
             <% piece = Map.get(@game.board, square) %>
+            <% file_index = Enum.find_index([:a, :b, :c, :d, :e, :f, :g, :h], fn f -> f == file end) %>
+            <% rank_index = rank - 1 %>
             <div class={[
               "w-12 h-12 flex items-center justify-center",
               background(file_index, rank_index)
@@ -28,8 +40,8 @@ defmodule ChessTrainerWeb.BoardLiveComponent do
 
   def update(%{fen: fen}, socket) do
     game = Chex.Parser.FEN.parse(fen)
-    IO.inspect(game.board)
-    {:ok, assign(socket, game: game)}
+    active_color = game.active_color
+    {:ok, assign(socket, game: game, active_color: active_color)}
   end
 
   defp background(file_index, rank_index) when rem(file_index + rank_index, 2) != 0,
